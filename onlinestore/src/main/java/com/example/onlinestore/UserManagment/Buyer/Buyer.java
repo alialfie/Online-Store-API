e32a53032156d23cd2375fae8a1b59ac429fc7d0
fatmaashraframadan <36447147+fatmaashraframadan@users.noreply.github.com>
2020-03-23 23:59:10 +0200

    Handling Null columns is Done.

--- onlinestore/src/main/java/com/example/onlinestore/UserManagment/Buyer/Buyer.java
+++ onlinestore/src/main/java/com/example/onlinestore/UserManagment/Buyer/Buyer.java
@@ -1,18 +1,24 @@
 package com.example.onlinestore.UserManagment.Buyer;
 
 import com.example.onlinestore.UserManagment.User;
+import com.sun.istack.NotNull;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Buyer extends User {
+    @NotNull
+    @NotEmpty
+   // @NotBlank
     private String adrress;
 
     public String getAdrress() {
         return adrress;
     }
 
-    public void setAdrress(String adrress) {
+    public void setAdrress( String adrress) {
         this.adrress = adrress;
     }
 }
--- onlinestore/src/main/java/com/example/onlinestore/UserManagment/User.java
+++ onlinestore/src/main/java/com/example/onlinestore/UserManagment/User.java
@@ -1,25 +1,41 @@
 package com.example.onlinestore.UserManagment;
 
+import com.sun.istack.NotNull;
+
 import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public abstract class User {
     @Id
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String username;
-
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String password;
-
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String email;
-
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String name;
-
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String age;
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
+    public void setUsername(  String username) {
         this.username = username;
     }
 
